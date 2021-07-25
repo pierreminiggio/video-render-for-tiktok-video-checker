0cdc8583011835ded311b6f2cbe0a2aaf2029178
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-25 10:47:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Install using composer :
+```
+composer require pierreminiggio/video-render-for-tiktok-video-checker
+```
---
+++ src/VideoRenderForTiktokVideoChecker.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PierreMiniggio\VideoRenderForTiktokVideoChecker;
+
+class VideoRenderForTiktokVideoChecker
+{
+
+    public function __construct(private string $spinnerApiUrl, private string $spinnerApiToken)
+    {
+    }
+
+    protected function getRenderedVideoUrl(string $tikTokUrl): ?string
+    {
+
+        $curl = curl_init($this->spinnerApiUrl . '/tiktok-video-file');
+
+        $authHeader = ['Content-Type: application/json' , 'Authorization: Bearer ' . $this->spinnerApiToken];
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $authHeader,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $tikTokUrl
+        ]);
+
+        $response = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if ($httpCode !== 200) {
+            return null;
+        }
+
+        if (! $response) {
+            return null;
+        }
+
+        $jsonResponse = json_decode($response, true);
+
+        if (! $jsonResponse) {
+            return null;
+        }
+
+        if (! isset($jsonResponse['id']) || ! isset($jsonResponse['hasRenderedFile'])) {
+            return null;
+        }
+
+        if ($jsonResponse['hasRenderedFile'] === false) {
+            return null;
+        }
+
+        return $this->spinnerApiUrl . '/render/' . $jsonResponse['id'];
+    }
+}
